5328e26e82cd285c97057df8bf4c904ac6713fcc
ShunichirouKamino <kamino.shunichirou@isid.co.jp>
2022-04-22 17:00:59 +0900

    setting.jsonにてmoduleを認識させる

--- hello_py/main.py
+++ hello_py/main.py
@@ -1,8 +1,12 @@
-import tqdm as td
+from tqdm import tqdm
+import numpy as np
 import time
 
 
 def main():
     print("Hello! Progress!")
-    for i in td(range(10)):
+    x = np.array([1, 2, 3])
+    print(x)
+
+    for i in tqdm(range(10)):
         time.sleep(1)
